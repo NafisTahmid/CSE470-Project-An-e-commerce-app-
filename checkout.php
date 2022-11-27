1139766fe6f6cec27997cd459e57a22efba1c1c9
NafisTahmid <114669634+NafisTahmid@users.noreply.github.com>
2022-11-27 18:19:07 +0600

    Add files via upload

--- checkout.php
+++ checkout.php
@@ -2,113 +2,33 @@
 
 session_start();
 
-if(isset($_POST['add_to_cart'])){
 
-    //if user has already added a product to cart
-    if(isset($_SESSION['cart'])){
+if(! empty($_SESSION['cart']) && isset($_POST['checkout'])){
 
-      $products_array_ids=array_column($_SESSION['cart'],"product_id");// [2,3,4,10,15]
-      //If product has already been added to cart or not
-      if(!in_array($_POST['product_id'],$products_array_ids)){
+//let user in  
 
-          $product_id=$_POST['product_id'];        
-  
-          $product_array = array(
-                        'product_id'=>$_POST['product_id'],
-                        'product_name'=>$_POST['product_name'],
-                        'product_price'=>$_POST['product_price'],
-                        'product_image'=>$_POST['product_image'],
-                        'product_quantity'=>$_POST['product_quantity']
-          );
-
-          $_SESSION['cart'][$product_id]=$product_array;
-
-      //product has already been added
-      }else{
-
-          echo '<script>alert("Product was already to cart");</script>';
-          //echo '<script>window.location="index.php";</script>';
-
-      }
-
-    //if this is the first product  
-    }else{
-
-
-      $product_id=$_POST['product_id'];
-      $product_name=$_POST['product_name'];
-      $product_price=$_POST['product_price'];
-      $product_image=$_POST['product_image'];
-      $product_quantity=$_POST['product_quantity'];
-
-      $product_array = array(
-        'product_id'=>$product_id,
-        'product_name'=>$product_name,
-        'product_price'=>$product_price,
-        'product_image'=>$product_image,
-        'product_quantity'=>$product_quantity
-      );
-
-      $_SESSION['cart'][$product_id]=$product_array;
-      // [ 2=>[], 3=>[], 5=>[] ]
-
-    //remove product from cart  
-    }
-
-     //calculate total
-    calculateTotalCart()  
-    
-//remove product from cart    
-}else if(isset($_POST['remove_product'])){
-
-  $product_id=$_POST['product_id'];
-  unset($_SESSION['cart'][$product_id]);
-
-}else if(isset($_POST['edit_quantity'])){
-
-      //we get id and quantity from the form
-      $product_id=$_POST['product_id'];
-      $product_quantity=$_POST['product_quantity'];
-
-      //get the product array from the session
-      $product_array=$_SESSION['cart'][$product_id];
-
-      //update product quantity
-      $old_quantity=$product_quantity;
-
-      //return array back its place
-      $_SESSION['cart'][$product_id]=$product_array;
-
-      //calculate total
-      calculateTotalCart();
-
-
-
+//send user to home page
 }else{
-
-  header('location: index.php');
+      //take user to the homepage
+    header('location: index.php');
 }
 
-
-
-
-function calculateTotalCart(){
-    $total=0;
-    foreach($_SESSION['cart'] as $key=>$value){
-        $product=$_SESSION['cart'][$key];
-        $price=$product['product_price'];
-        $quantity=$product['product_quantity'];
-
-        $total=$total+($price*$quantity);
-
-
-    }
-
-    $_SESSION['total']=$total;
-
-}
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -190,78 +110,84 @@ function calculateTotalCart(){
       </div>
     </nav>
 
-    <!--Cart-->
-    <section class="cart contariner my-5 py-5">
-      <div class="container mt-5">
-        <h2 class="font-weight-bolde">Your Cart</h2>
-        <hr />
+    <!--Checkout-->
+    <section class="my-5 py-5">
+      <div class="container text-center mt-3 pt-5">
+        <h2 class="form-weight-bold">Check Out</h2>
+        <hr class="mx-auto" />
       </div>
+      <div class="mx-auto container">
+        <form id="checkout-form" method="POST" action="server/place_order.php">
+          <div class="form-group checkout-small-element">
+            <label>Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="checkout-name"
+              name="name"
+              placeholder="Name"
+              required
+            />
+          </div>
+          <div class="form-group checkout-small-element">
+            <label>Email</label>
+            <input
+              type="text"
+              class="form-control"
+              id="checkout-email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </div>
 
-      <table class="mt-5 pt-5">
-        <tr>
-          <th>Product</th>
-          <th>Quantity</th>
-          <th>Subtotal</th>
-        </tr>
+          <div class="form-group checkout-small-element">
+            <label>Phone</label>
+            <input
+              type="tel"
+              class="form-control"
+              id="checkout-phone"
+              name="phone"
+              placeholder="Phone"
+              required
+            />
+          </div>
+          <div class="form-group checkout-small-element">
+            <label>City</label>
+            <input
+              type="text"
+              class="form-control"
+              id="checkout-city"
+              name="city"
+              placeholder="City"
+              required
+            />
+          </div>
 
-        <?php foreach($_SESSION['cart'] as $key=>$value){?>
+          <div class="form-group checkout-large-element">
+            <label>Address</label>
+            <input
+              type="text"
+              class="form-control"
+              id="checkout-address"
+              name="address"
+              placeholder="Address"
+              required
+            />
+          </div>
 
-        <tr>
-          <td>
-            <div class="product-info">
-              <img src="assets/imgs/<?php echo $value['product_image']; ?>"/>
-              <div>
-                <p><?php echo $value['product_name'];?></p>
-                <small><span>$</span><?php echo $value['product_price']; ?></small>
-                <br>
-             
-                <form method="POST" action="cart.php">
-                  <input type="hidden" name="product_id" value="<?php echo $value['product_id'];?>"/>
-                  <input type="submit" name="remove_product" class="remove-btn" value="remove"/>
-                </form>
-              </div>
-            </div>
-          </td>
-
-          <td>
-            
-            <form method="POST" action="cart.php">
-                <input type="hidden" name="product_id" value="<?php echo $value['product_id']; ?>"/>
-                <input type="number" name="product_quantity" value="<?php echo $value['product_quantity']; ?>"/>
-                <input type="submit" class="edit-btn" value="edit" name="edit_quantity"/>
-            </form>
-            
-          </td>
-
-          <td> 
-            <span>$</span>  
-            <span class="product-price"><?php echo $value['product_quantity'] * $value['product_price']; ?> </span>
-          </td>
-        </tr>
-
-        <?php } ?>
-
-      </table>
-
-      <div class="cart-total">
-        <table>
-          <!--<tr>
-            <td>Subtotal</td>
-            <td>$155</td>
-          </tr> -->
-          <tr>
-            <td>Total</td>
-            <td>$<?php echo $_SESSION['total']; ?></td>
-          </tr>
-        </table>
-      </div>
-
-      <div class="checkout-container">
-        <form method="POST" action="checkout.php">
-          <input type="submit" class="btn checkout-btn" value="Checkout" name="checkout">
+          <div class="form-group checkout-btn-container">
+            <p>Total amount: $<?php echo $_SESSION['total']; ?></p>
+            <input
+              type="submit"
+              class="btn"
+              id="checkout-btn"
+              name="place_order"
+              value="Place Order"
+            />
+          </div>
         </form>
       </div>
-      
     </section>
 
     <!--Footer-->
@@ -347,4 +273,3 @@ function calculateTotalCart(){
     ></script>
   </body>
 </html>
-  
--- single_product.php
+++ single_product.php
@@ -113,7 +113,10 @@ else {
 
         <?php while($row=$product->fetch_assoc()){ ?>
 
-         
+       
+
+             
+        
         <div class="col-lg-5 col-md-6 col-sm-12">
           <img class="img-fluid w-100 pb-1" src="assets/imgs/<?php echo $row['product_image']; ?>" id="mainImg" />
           <div class="small-img-group">
@@ -154,25 +157,27 @@ else {
           <h6>Men's Shoes</h6>
           <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
           <h2>$<?php echo $row['product_price']; ?></h2>
+          <form method="POST" action="cart.php">
+          <input type="hidden" name="product_id" value="<?php echo $row['product_id']; ?>"/>
+          <input type="hidden" name="product_image" value="<?php echo $row['product_image']; ?>"/>
+          <input type="hidden" name="product_name" value="<?php echo $row['product_name']; ?>"/>
+          <input type="hidden" name="product_price" value="<?php echo $row['product_price']; ?>"/>
 
-          <form method = "POST" action="cart.php">
-            <input type = "hidden" name="product_image" value="<?php echo $row['product_image']; ?>"/>
-            <input type="hidden" name="product_name" value="<?php echo $row['product_name']; ?>"/>
-            <input type="hidden" name="product_name" value="<?php echo $row['product_price']; ?>"/>
+
 
           <input type="number" name="product_quantity"  value="1" />
           <button class="buy-btn" type="submit" name="add_to_cart">Add to Cart</button>
         </form>
+         
+  
 
           
           <h4 class="mt-5 mb-5">Product details</h4>
           <span>
-          <?php echo $row['product_description']; ?>
+          <?php echo $row['product_description'];?>
           </span>
         </div>
-
-
-
+        
         <?php } ?>
       </div>
     </section>
